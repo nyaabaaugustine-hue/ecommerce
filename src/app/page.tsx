c2ba9934121df0c7692054e72204d691f1faa39b
Augustine Nyaaba <nyaabaaugustine@gmail.com>
2026-03-20 14:23:19 +0000

    implement a smooth slide in at the slider level , with a product adverti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,8 @@ import {
   Lock,
   ShieldAlert,
   Plus,
-  Store
+  Store,
+  ArrowUpRight
 } from 'lucide-react';
 import { Badge } from '@/components/ui/badge';
 import { Card, CardContent } from '@/components/ui/card';
